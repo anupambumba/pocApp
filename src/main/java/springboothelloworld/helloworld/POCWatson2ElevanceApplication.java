c6287af1507d4c2a466f3e358977d40db94b833e
Anupam Sarkar <anupam.sarkar@in.ibm.com>
2023-11-24 21:08:55 +0530

    Scanner add

--- src/main/java/springboothelloworld/helloworld/POCWatson2ElevanceApplication.java
+++ src/main/java/springboothelloworld/helloworld/POCWatson2ElevanceApplication.java
@@ -1,9 +1,13 @@
 package springboothelloworld.helloworld;
 
-import org.springframework.boot.SpringApplication; 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import springboothelloworld.helloworld.controller.POCWatson2ElevanceController;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses=POCWatson2ElevanceController.class)
 public class POCWatson2ElevanceApplication {
 
 	public static void main(String[] args) {
