04963773ef4f67c1af92289cdc4dc470e9cffd43
Anupam Sarkar <anupam.sarkar@in.ibm.com>
2023-11-27 21:09:01 +0530

    Added Origin

--- src/main/java/springboothelloworld/helloworld/POCWatson2ElevanceApplication.java
+++ src/main/java/springboothelloworld/helloworld/POCWatson2ElevanceApplication.java
@@ -2,7 +2,11 @@ package springboothelloworld.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import springboothelloworld.helloworld.controller.POCWatson2ElevanceController;
 
@@ -13,5 +17,13 @@ public class POCWatson2ElevanceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(POCWatson2ElevanceApplication.class, args);
 	}
-
+	 @Bean
+	   public WebMvcConfigurer corsConfigurer() {
+	      return new WebMvcConfigurerAdapter() {
+	         @Override
+	         public void addCorsMappings(CorsRegistry registry) {
+	            registry.addMapping("/poc").allowedOrigins("*");
+	         }
+	      };
+	 }
 }
--- src/main/java/springboothelloworld/helloworld/controller/POCWatson2ElevanceController.java
+++ src/main/java/springboothelloworld/helloworld/controller/POCWatson2ElevanceController.java
@@ -10,6 +10,7 @@ import org.apache.wink.json4j.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +23,7 @@ import springboothelloworld.helloworld.pojo.IDCardAuthorization;
 import springboothelloworld.helloworld.pojo.IDCardImage;
 
 @RestController
+@RequestMapping(path = "/poc")
 public class POCWatson2ElevanceController {
 	@GetMapping(path="/")
 	public String home() throws JsonProcessingException
